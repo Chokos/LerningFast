a0c3d3e372111491527382927c4afe17ae7f0b23
Kwnstantinos K <notImplementedYetOnMyPcTesting@gmail.com>
2025-09-06 13:04:28 +0300

    basic controls

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,29 @@
 import {dummyData} from "./data/todos.ts";
+import Todoitem from "./componets/Todoitem.tsx";
 
 
 function App() {
-  return (
-    <main className="learningfast">
-      <h2 className = "font-bold text-3xl text-center" > LEARNING</h2>
 
-        <div className="max-w-lg mx-auto">
-            <div className ="space-y-2">
-                {dummyData.map(todo =>(
-                    <p key={todo.id} className="text-lg">
-                        {todo.title}
-                    </p>
-                ))}
+    function setCompleted(id:number,completed:boolean){
+        alert(`Todo with id ${id} is ${completed ? 'completed' : 'not completed'}`);
+    }
 
+    return (
+        <main className="py-10 h-screen space-y-5">
+          <h2 className = "font-bold text-3xl text-center" > LEARNING</h2>
+            <div className="max-w-lg mx-auto bg-slate-100 rounded-md p-5">
+                <div className ="space-y-2">
+                    {dummyData.map(todo =>(
+                        <Todoitem
+                            key={todo.id}
+                            todo={todo}
+                            onComplete={setCompleted}
+                        />
+                    ))}
+                </div>
             </div>
-        </div>
-    </main>
-  )
-}
+        </main>
+      );
+    }
 
-export default App
+    export default App;
--- src/componets/Todoitem.tsx
+++ src/componets/Todoitem.tsx
@@ -1,6 +1,22 @@
-export default function TodoItem() {
+import type {todo} from "../types/todo";
 
+interface TodoItemProps {
+    todo:todo;
+    onComplete: (id:number,completed:boolean) => void;
+}
+
+export default function TodoItem({todo,onComplete}:TodoItemProps) {
     return (
-        <div>TodoItem</div>
+        <div>
+            <label className="flex items-center gap-2 border rounded-md p-2 border-gray-400 bg-white hover:bg-slate-50">
+                <input type="checkbox"
+                       checked={todo.completed}
+                       onChange={(e)=> onComplete(todo.id,e.target.checked)}
+                       className="scale-125"/>
+                <span className={todo.completed ? "opacity-50 cursor-pointer" : "opacity-100"}>
+                    {todo.title}
+                </span>
+            </label>
+        </div>
     )
 }

b36f57cea60a979e8ff722d65f31987d99b8d37f
Kwnstantinos K <notImplementedYetOnMyPcTesting@gmail.com>
2025-09-06 13:36:53 +0300

    basic state

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import {dummyData} from "./data/todos.ts";
 import Todoitem from "./componets/Todoitem.tsx";
+import {useState} from "react";
 
 
 function App() {
+    const [todos, setTodos ]= useState(dummyData);
 
     function setCompleted(id:number,completed:boolean){
-        alert(`Todo with id ${id} is ${completed ? 'completed' : 'not completed'}`);
+       setTodos((prevTodos)=>
+           prevTodos.map(todo =>
+               (todo.id === id ? {...todo,completed }:todo))
+       );
     }
 
     return (
@@ -13,7 +18,7 @@ function App() {
           <h2 className = "font-bold text-3xl text-center" > LEARNING</h2>
             <div className="max-w-lg mx-auto bg-slate-100 rounded-md p-5">
                 <div className ="space-y-2">
-                    {dummyData.map(todo =>(
+                    {todos.map(todo =>(
                         <Todoitem
                             key={todo.id}
                             todo={todo}
